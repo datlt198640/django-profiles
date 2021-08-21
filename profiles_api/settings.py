677b58ce1d6dbad8821eaea6e8bfd487623f1124
datlt198640 <dat.lt198640@gmail.com>
2021-08-21 12:15:19 -0400

    update_model

--- profiles_api/settings.py
+++ profiles_api/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+AUTH_USER_MODEL = 'core.UserProfile'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
