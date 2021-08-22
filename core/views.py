540a153beff865558b459789a3f83694af72bd71
datlt198640 <dat.lt198640@gmail.com>
2021-08-22 17:22:34 -0400

    login_api

--- core/views.py
+++ core/views.py
@@ -6,6 +6,8 @@ from rest_framework import viewsets
 from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
 from core import permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 # Create your views here.
 ...
 
@@ -60,6 +62,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
+class UserLoginApiView(ObtainAuthToken):
+     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 # --------------------------------------------------------
 class HelloApiView(APIView):
