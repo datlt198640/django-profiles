ce10700874f0f943a2aa90b3ea452101227d56a0
datlt198640 <dat.lt198640@gmail.com>
2021-08-22 15:29:12 -0400

    serializer

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from core import serializers
+from rest_framework import status
 
 # Create your views here.
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializers
     def get(self, request):
         """Returns a list of APIView features"""
 
@@ -15,3 +17,29 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs',
         ]
         return Response({'message':'hello','an_apiview':an_apiview})
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello{name}'
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+            )
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial update of object"""
+
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+
+        return Response({'method': 'DELETE'})
