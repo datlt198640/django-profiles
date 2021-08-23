3e9ce0174ee0e238ec9d611dce94dc011133a3b2
datlt198640 <dat.lt198640@gmail.com>
2021-08-22 22:21:03 -0400

    profile_feed

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, ProfileFeedItem
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(ProfileFeedItem)
--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from core import permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
@@ -64,6 +65,19 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
 class UserLoginApiView(ObtainAuthToken):
      renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated
+    )
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
 
 # --------------------------------------------------------
 class HelloApiView(APIView):
